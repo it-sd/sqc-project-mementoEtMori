758b96d50307a09c9fe06a1839510437f0e3ae4f
Braden Walbeck <bwalbeck@student.cvtc.edu>
2023-02-21 21:38:27 -0600

    Added style and tried to add display to list page

--- server.js
+++ server.js
@@ -51,6 +51,38 @@ express()
     res.render('pages/about')
   })
   .get('/health', function (req, res) {
-  res.status(200).send('Healthy')
+    const result = query('SELECT charityName FROM charities;')
+    if (result.length === 0) {
+      res.status(500).send('Unhealthy')
+    } else {
+      res.status(200).send('Healthy')
+    }
+  })
+  .get('/list', async function (req, res) {
+    const charityName = await queryAllCharities('charityName')
+    const category = await queryAllCharities('category')
+    const state = await queryAllCharities('state')
+    
+    const entries = {
+      charityName: charityName.entries,
+      category: category.entries,
+      state: state.entries,
+      table: ''
+    }
+    res.render('pages/list', entries)
+  })
+  .get('/list/:table', async function (req, res) {
+    const entries = await queryAllCharities(req.params.table)
+    const data = {
+      table: req.params.table,
+      charityName: [],
+      category: [],
+      state: []
+    }
+
+    res.render('pages/list', data)
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+
+
